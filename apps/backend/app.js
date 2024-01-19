6fe1c89f924bb8c740e5614bc3992121c662dade
Surbhi Vandana <svandu08@gmail.com>
2024-01-19 15:47:37 +0530

    add question api done

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -4,7 +4,9 @@ const server = require("./config/mongodb");
 
 // router
 const adminRouter = require("./routes/admin.route");
+const questionRouter = require("./routes/question.route")
 
 server.app.use("/api/v1/admin", adminRouter);
+server.app.use("/api/v1/question", questionRouter)
 
 server.listenToServer();
