37c93695c24dc7988d1de21415d8ca94e7e9b0e8
Subrat kumar jena <subratj611@gmail.com>
2024-01-21 17:01:34 +0530

    responsive ui

--- apps/web-assessment/src/components/SideNav.jsx
+++ apps/web-assessment/src/components/SideNav.jsx
@@ -7,27 +7,40 @@ import { sideNavLinks, sideNavLinksLearn } from "@/lib";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { IconBuddyProfileIcon } from "./icon";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
-const SideNav = () => {
+const SideNav = (props) => {
+  const { isNavShow, setIsNavShow } = props;
   const pathname = usePathname();
+
   return (
     <div
       className={cn(
-        "pb-12, h-full max-w-[240px] bg-gray-50 border-r border-gray-300 hidden md:inline-block"
+        "pb-12, h-full max-w-[240px] bg-gray-50 border-r border-gray-300 md:inline-block",
+        `${isNavShow ? "absolute top-0 left-0" : "hidden"}`
       )}
     >
       <div className="space-y-4 px-3 py-6 h-[90vh] flex flex-col justify-between">
         <div>
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            E-Learning Hub
-          </h2>
+          <div className="flex items-center justify-between mb-2 pl-2">
+            <h2 className="text-lg font-semibold tracking-tight">
+              E-Learning Hub
+            </h2>
+            <Button
+              variant="ghost"
+              className="px-2 bg-transparent focus:bg-gray-100 rounded-full md:hidden"
+              onClick={() => setIsNavShow((prev) => !prev)}
+            >
+              <Cross2Icon className="text-gray-600 w-5 h-5" />
+            </Button>
+          </div>
           <div className="space-y-2 mt-5">
             {sideNavLinks.map((item, i) => {
               const { label, icon, source } = item;
               return (
                 <Link href={source} key={`${label}-${i}`}>
                   <Button
-                    variant={pathname === source ? "default" : "ghost"}
+                    variant={pathname.includes(source) ? "default" : "ghost"}
                     className="w-full flex items-center justify-start my-1"
                   >
                     {icon}
