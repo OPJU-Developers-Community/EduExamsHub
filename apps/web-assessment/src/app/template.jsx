ee38a7f557dac65e8dfcbf5c6f11fd7a6a6b4530
Subrat kumar jena <subratj611@gmail.com>
2024-01-21 17:00:44 +0530

    common template for every page develop

--- apps/web-assessment/src/app/template.jsx
+++ apps/web-assessment/src/app/template.jsx
@@ -1,10 +1,25 @@
+"use client";
 import SideNav from "@/components/SideNav";
+import { Button } from "@/components/ui/button";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export default function DefaultTemplate({ children }) {
+  const [isNavShow, setIsNavShow] = useState(false);
+
   return (
     <main className="h-screen flex">
-      <SideNav />
-      <section className="h-full w-full py-10 px-5">{children}</section>
+      <SideNav isNavShow={isNavShow} setIsNavShow={setIsNavShow} />
+      <section className="h-full w-full pt-6 px-5">
+        <Button
+          variant="ghost"
+          className="px-2 bg-transparent focus:bg-gray-100 rounded-full md:hidden"
+          onClick={() => setIsNavShow((prev) => !prev)}
+        >
+          <HamburgerMenuIcon className="text-gray-600 h-5 w-5" />
+        </Button>
+        {children}
+      </section>
     </main>
   );
 }
