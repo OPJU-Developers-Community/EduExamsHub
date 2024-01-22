15c1c052295fd4535fc1de32698e30268829fd8e
Subrat kumar jena <subratj611@gmail.com>
2024-01-22 20:30:14 +0530

    classname add

--- apps/web-assessment/src/components/LoadingBtn.jsx
+++ apps/web-assessment/src/components/LoadingBtn.jsx
@@ -1,9 +1,10 @@
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
-const LoadingBtn = ({ children }) => {
+const LoadingBtn = ({ children, classname }) => {
   return (
-    <Button disabled>
+    <Button disabled className={cn(classname)}>
       <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
       {children ? children : "Please wait"}
     </Button>
