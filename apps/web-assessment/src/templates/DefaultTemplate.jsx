755a192a46407a9710a1b33d34a028074b7120a8
Subrat kumar jena <subratj611@gmail.com>
2024-01-22 11:47:21 +0530

    minor changes

--- apps/web-assessment/src/templates/DefaultTemplate.jsx
+++ apps/web-assessment/src/templates/DefaultTemplate.jsx
@@ -14,15 +14,17 @@ export default function DefaultTemplate({ children }) {
   return (
     <main className={`h-screen flex ${inter.className}`}>
       <SideNav isNavShow={isNavShow} setIsNavShow={setIsNavShow} />
-      <section className="h-full w-full pt-6 px-5">
-        <Button
-          variant="ghost"
-          className="px-2 bg-transparent focus:bg-gray-100 rounded-full md:hidden"
-          onClick={() => setIsNavShow((prev) => !prev)}
-        >
-          <HamburgerMenuIcon className="text-gray-600 h-5 w-5" />
-        </Button>
-        <div className="pt-5 px-2 md:px-10">{children}</div>
+      <section className="h-full w-full">
+        <div className="h-[50px] w-full flex items-center px-2 bg-gray-50 border-b border-gray-300">
+          <Button
+            variant="ghost"
+            className="px-2 bg-transparent focus:bg-gray-100 rounded-full md:hidden"
+            onClick={() => setIsNavShow((prev) => !prev)}
+          >
+            <HamburgerMenuIcon className="text-gray-600 h-5 w-5" />
+          </Button>
+        </div>
+        <div className="pt-10 px-4 md:px-10">{children}</div>
       </section>
     </main>
   );
