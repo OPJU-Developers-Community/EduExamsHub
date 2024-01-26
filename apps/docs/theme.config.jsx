a524d26b3a4e4870c02f84a6f101e17def00f8c7
Subrat kumar jena <subratj611@gmail.com>
2024-01-26 20:28:07 +0530

    docs nextra add

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const config = {
+  logo: <span>E-Learning Hub</span>,
+  project: {
+    link: "https://github.com/subrat611/OneStopDoc",
+  },
+  docsRepositoryBase: "https://github.com/subrat611/OneStopDoc",
+  footer: {
+    text: "@2024 Subrat Jena",
+  },
+};
+
+export default config;
