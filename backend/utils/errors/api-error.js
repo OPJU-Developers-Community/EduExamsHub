5ec99ad325f1bf36741f5743afcd48cbbb0f10ea
Subrat kumar jena <subratj611@gmail.com>
2024-01-28 23:39:21 +0530

    update to AppError

--- backend/utils/errors/api-error.js
+++
@@ -1,9 +0,0 @@
-class ApiError extends Error {
-    constructor(message, statusCode) {
-        super(message),
-        this.statusCode = statusCode,
-        this.explanation = message
-    }
-}
-
-module.exports = ApiError;
