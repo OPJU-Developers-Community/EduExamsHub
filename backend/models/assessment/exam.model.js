b0b2b2a59c14df80fe91f3dab5bb9a343e8d4365
Subrat kumar jena <subratj611@gmail.com>
2024-01-28 22:08:12 +0530

    feat: Joi validation and model field update

--- backend/models/assessment/exam.model.js
+++ backend/models/assessment/exam.model.js
@@ -1,46 +1,57 @@
-const {mongoose, Schema} = require("mongoose")
+const Joi = require("joi");
+const { mongoose, Schema } = require("mongoose");
 
-const examSchema = new Schema (
-    {
-        course_code: {
-            type: String,
-            require: true,
-            unique: true
-        },
-        course: {
-            type: String,
-            require: true
-        },
-        semester: {
-            type: Number,
-            require: true,
-        },
-        subject: {
-            type: String,
-            require: true,
-        },
-        max_marks: {
-            type: Number,
-            require: true,
-        },
-        exam_rules: {
-            type: String,
-            require: true,
-        },
-        questions: {
-            type: [String],
-        },
-        start_date: {
-            type: Date,
-            require: true,
-        },
-        end_date: {
-            type: Date,
-            require: true,
-        }
-    }
-)
+const examSchema = new Schema({
+  course_code: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  course: {
+    type: String,
+    require: true,
+  },
+  semester: {
+    type: Number,
+    require: true,
+  },
+  subject: {
+    type: String,
+    require: true,
+  },
+  max_marks: {
+    type: Number,
+    require: true,
+  },
+  exam_rules: {
+    type: String,
+    require: true,
+  },
+  start_date: {
+    type: Date,
+    require: true,
+  },
+  end_date: {
+    type: Date,
+    require: true,
+  },
+});
 
-const examModel = mongoose.model("Exam", examSchema)
+const examModel = mongoose.model("Exam", examSchema);
 
-module.exports = examModel;
+const validateExamReqBody = (reqBody) => {
+  const schema = Joi.object({
+    course_code: Joi.string().required(),
+    course: Joi.string().required(),
+    semester: Joi.number().integer().positive().min(1).max(8).required(),
+    subject: Joi.string().required(),
+    max_marks: Joi.number().integer().positive().required(),
+    exam_rules: Joi.string(),
+    start_date: Joi.date().required(),
+    end_date: Joi.date().required(),
+  });
+
+  return schema.validate(reqBody);
+};
+
+module.exports = { examModel, validateExamReqBody };
